e5351dc1ddd1a2beb911d4a18652dc98b19fff28
Thomas Rudrauf <thomas.rudrauf@gmail.com>
2018-03-27 12:21:04 +0200

    fix Px() if document is not ready

--- packages/ui/basic/Px.js
+++ packages/ui/basic/Px.js
@@ -1,4 +1,22 @@
-var initialWidth = document.body.clientWidth || window.innerWidth;
+/**
+ * Px - Echelle de mesure
+ * @constructor
+ * @param {int} px - dimension de référence sur un écran de 1024px
+ * @returns float
+ * @author Thomas Rudrauf
+ * @license @license Apache-2.0
+ * @date   27/03/2018
+ */
+
+var initialWidth = 0;
+
+try {
+	initialWidth = document.body.clientWidth;
+}
+catch(err) {
+	initialWidth = window.innerWidth;
+}
+
 var Px = function(px) {
 	return px / (1024 / initialWidth)+'px';
 };
